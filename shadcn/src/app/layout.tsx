d8701341e05885f804f4d51724e4269ab4e48b83
yuribodo <yemotalara@gmail.com>
2024-10-24 18:11:21 -0400

    refactor: add applies to global.css and remove content from page.tsx and add a nav to layout

--- shadcn/src/app/layout.tsx
+++ shadcn/src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <h1>Shadcn Practice</h1>
+        </nav>
         {children}
       </body>
     </html>
